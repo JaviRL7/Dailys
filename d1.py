fdd18cecca533878858dfcda06edf503f9958235
Javier Rodriguez <javier.rodriguez.lopez@iesdonana.org>
2022-04-28 17:42:33 +0200

    Segunda version

--- d1.py
+++ d1.py
@@ -1,6 +1,6 @@
 def daily(lista,k):
     for i in lista:
         for j in lista[:1]:
-            if i+j == k:
+            if i+j == k and i is not j:
                 return True
     return False
