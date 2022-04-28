9751417c97086f1e3539eeb024eef40b059bc71a
JaviRL7 <Jrlsanlucar11@gmail.com>
2022-04-28 18:15:10 +0200

    Segunda version daily

--- d1.py
+++ d1.py
@@ -1,6 +1,6 @@
 def daily(lista,k):
-    for i in lista:
-        for j in lista[:1]:
-            if i+j == k and i is not j:
+    for i,m in enumerate(lista):
+        for j in lista[:i]:
+            if m+j == k and i is not j:
                 return True
     return False
